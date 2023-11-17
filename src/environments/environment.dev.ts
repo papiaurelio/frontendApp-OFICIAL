745bc771834422cdf5b4582597015fc35b53c4d6
Aurelio <byrondereyes@gmail.com>
2023-11-16 23:42:21 -0600

    implementacion guards

---
+++ src/environments/environment.dev.ts
@@ -0,0 +1,19 @@
+export const environment = {
+  production: false,
+  name: 'dev',
+  firebase: {
+    config : {
+      apiKey: "AIzaSyB4-ampw4RnIO_yoGj8m1OqFGnMJKTjCmk",
+      authDomain: "ecommerce-4e063.firebaseapp.com",
+      projectId: "ecommerce-4e063",
+      storageBucket: "ecommerce-4e063.appspot.com",
+      messagingSenderId: "291434632434",
+      appId: "1:291434632434:web:2d33c50fcfc102586dec6b"
+    }
+  },
+  actionCodeSettings: {
+    url: 'http://localhost:5200/profile/new',
+    handleCodeInApp: true
+  },
+  url: 'http://localhost:4204'
+};
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,19 @@
+export const environment = {
+  production: true,
+  name: 'prod',
+  firebase: {
+    config : {
+      apiKey: "AIzaSyB4-ampw4RnIO_yoGj8m1OqFGnMJKTjCmk",
+      authDomain: "ecommerce-4e063.firebaseapp.com",
+      projectId: "ecommerce-4e063",
+      storageBucket: "ecommerce-4e063.appspot.com",
+      messagingSenderId: "291434632434",
+      appId: "1:291434632434:web:2d33c50fcfc102586dec6b"
+    }
+  },
+  actionCodeSettings: {
+    url: 'https://ecommerce-e4e6c.web.app/profile/new',
+    handleCodeInApp: true
+  },
+  url: 'http://localhost:4204'
+};
